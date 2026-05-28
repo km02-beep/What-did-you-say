52d0370e07c969781bb4ed9cf28c156caa16bda6
km02-beep <kamanlam99@gmail.com>
2026-05-28 12:22:32 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,55 @@
+// data.js
+const characterDataList = [
+    //31-03-2026
+    {
+        img: "images/h.png",
+        story: [
+            "31-03-2026<br>9:38 - 9:44 • 73% <br>Zullen we dat doen in zo'n bakje? Nee, dat is niet lekker. 8 uur in een deeg gebakken uitjes. <br><br> <span class='clickTip'>Click for translation </span>",
+            "EN <br>Shall we do that in one of those little containers? No, that isn't tasty. Onions fried in batter for 8 hours.<br><br><span class='clickTip'>Click for translation </span>",
+            "CN <br>我們用那種小容器裝好嗎？不行，那樣不好吃。洋蔥裹麵糊炸8小時。<br><br><span class='clickTip'>Click for translation </span>",
+        ]
+    },
+    {
+        img: "images/h.png",
+        story: [
+            "31-03-2026<br>2:26 - 3:36 • 43% <br> Het anders op een nacht als je op einde van de dag zit. Maar dat is echt niet zo ver. Ja, ook nog wat te drinken. <br><br> <span class='clickTip'>Click for translation </span>",
+            "EN <br>It's different at night when you're at the end of the day. But that's really not that far. Yes, something to drink too. <br><br> <span class='clickTip'>Click for translation </span>",
+            "CN <br>到了晚上，一天結束的時候，感覺就不一樣了。不過其實也沒那麼遠。對，也得喝點東西。 <br><br> <span class='clickTip'>Click for translation </span>",
+        ]
+    },
+    {
+        img: "images/h.png",
+        story: [
+            "31-03-2026<br>4:46 - 9:03 • 68% <br> Hoe weet je dat? We kunnen een beetje tekenen. <br><br> <span class='clickTip'>Click for translation </span>",
+            "EN <br>How do you know that? We can draw a little.<br><br> <span class='clickTip'>Click for translation </span>",
+            "CN <br>你怎麼知道的？我們可以畫一下。 <br><br> <span class='clickTip'>Click for translation </span>",
+        ]
+    },
+    {
+        img: "images/h.png",
+        story: [
+            "31-03-2026<br>9:51 - 11:11 • 60% <br> Kip jij? Oh nee, Kip gaat met mij toe. Dan moet je dus 8 uur in voorraad wel zien. <br><br> <span class='clickTip'>Click for translation </span>",
+            "EN <br>Chicken, you? Oh no, Chicken is coming with me. So then you'll just have to see if it's in stock for 8 hours.<br><br> <span class='clickTip'>Click for translation </span>",
+            "CN <br>雞肉，你呢？喔不，雞肉要跟我一起來。那你得看看8小時內有沒有貨。<br><br> <span class='clickTip'>Click for translation </span>"
+        ]
+    },
+    {
+        img: "images/h.png",
+        story: [
+            "31-03-2026<br> 11:12 - 11:38 • 65% <br> Die moet je ook met al jou niet overnemen. Nee, dat gaan we gewoon bestellen. Nee gaan gewoon Nee, dat Bestellen? Op een rack. Ja.<br><br> <span class='clickTip'>Click for translation </span>",
+            "EN <br>You shouldn't take that over either, with all you. No, we're just going to order that. No, just going to. No, that. Ordering? On a rack. Yes. <br><br> <span class='clickTip'>Click for translation </span>",
+            "CN <br>你也不該帶著它過來，你一個人。不，我們還是點那個吧。不，就點那個。不，那個。點那個？放在架子上。是的。<br><br> <span class='clickTip'>Click for translation </span>"
+        ]
+    },
+
+    //15-04-2026
+    {
+        img: "images/h.png",
+        story: [
+            "15-04-2026 <br> 6:30 - 7:00 • 79% <br> Mijn vrouw is opgepakt. <br><br> <span class='clickTip'>Click for translation </span>",
+            "EN <br> My wife has been arrested. <br><br> <span class='clickTip'>Click for translation </span>",
+            "CN <br>我妻子被捕了。<br><br> <span class='clickTip'>Click for translation </span>"
+        ]
+    },
+
+];
